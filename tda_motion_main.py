46c6aff88efbc7882f3448b0e2b6a0c66c986fa3
Sahil <sdhawan173@pm.me>
2024-01-23 00:02:50 -0500

    better organization, new functions

---
+++ tda_motion_main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import image_generator as ig
+
+
+width = 100
+height = width
+sample_size = 50
+motion_name = 'circle (triangular generation)'
+ig.generate_gif(motion_name)
+input('wait')
+start_point = 0
+end_point = width
+domain = np.linspace(start_point, end_point, num=sample_size)
+x_values = [int((width / 3) * (1 + point)) for point in np.cos(domain)]
+y_values = [int((width / 3) * (1 + point)) for point in np.sin(domain)]
+radius = 5
+color = [255, 0, 0]
+png_collection = ig.generate_data_samples(motion_name, x_values, y_values,
+                                          width, height, radius, color,
+                                          save_png=False)
+# ig.generate_gif(png_collection)
